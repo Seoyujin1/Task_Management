2c7167618191d6e1ff7f0eb87444b190aedfd3ae
SeoYuJin <104366642+Seoyujin1@users.noreply.github.com>
2022-05-12 23:04:51 +0900

    Update task.h

--- task.h
+++ task.h
@@ -15,8 +15,8 @@ int deleteTask(Task *t); // 과제 삭제
 void listTask(Task *t[], int count); //과제 목록 조회
 void readTask(Task t); // 과제 조회
 int selectDataNo(Task *t[], int count); // 과제 삭제나 수정 시에 과제 번호 선택
-void updateState(Task *t); // 과제 상태 수정하기 (시작 전 : 1, 진행 중 : 2, 완료 : 3)
-void saveData(Task *t, int count); // 파일 저장
-int loadData(Task *t); // 파일 불러오기
-void searchClassName(Task *t, int count); // 과제를 수업 이름으로 검색
-void searchDate(Task *t, int count); // 과제를 마감 기한으로 검색
+void updateState(Task *t, int count); // 과제 상태 수정하기 (시작 전 : 1, 진행 중 : 2, 완료 : 3)
+void saveData(Task *t[], int count); // 파일 저장
+int loadData(Task *t[]); // 파일 불러오기
+void searchClassName(Task *t[], int count); // 과제를 수업 이름으로 검색
+void searchDate(Task *t[], int count); // 과제를 마감기한으로 검색
